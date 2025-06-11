b0e1b7ef1577def81807852e093325742967b4bd
Lumineskz <shubhamsulabh6@gmail.com>
2025-06-11 22:08:40 +0545

    Some scripts to make it work, but not functional

--- Scripts/script-admin.js
+++ Scripts/script-admin.js
@@ -6,3 +6,32 @@ const hamburgerMenu = document.querySelector('.hamburger-menu');
 hamburgerMenu.addEventListener('click', () => {
     mobileNav.classList.toggle('open');
 });
+
+// For Making menu add items functional
+const addItemButton = document.querySelector('.add-item-btn');
+
+addItemButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    const menuItem = document.querySelector('#menuItem').value;
+    const itemPrice = document.querySelector('#itemPrice').value;
+    const itemDescription = document.querySelector('#itemDescription').value;
+    const itemImage = document.querySelector('#itemImage').value;
+    const itemCategory = document.querySelector('#itemCategory').value;
+
+    // Here you can add your logic to add the item to the menu
+
+    // Optionally, clear the form fields
+    document.querySelector('#menuItem').value = '';
+    document.querySelector('#itemPrice').value = '';
+    document.querySelector('#itemDescription').value = '';
+    document.querySelector('#itemImage').value = '';
+    document.querySelector('#itemCategory').value = '';
+
+    console.log('Adding item:', {
+        menuItem,
+        itemPrice,
+        itemDescription,
+        itemImage,
+        itemCategory
+    });
+});
